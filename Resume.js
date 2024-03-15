50c41b2f8a8df09c0c76072e52252f0776242ad4
parmar-dharmik <163393937+parmar-dharmik@users.noreply.github.com>
2024-03-15 14:14:44 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,46 @@
+function  slider() {
+let slider = document.getElementById("slide");
+if (slide.style.width == "150px") {
+    slide.style.width = "0px";
+    opacity.style.filter = "brightness(100%)";
+}else{
+    slide.style.width = "150px";
+    opacity.style.filter = "brightness(50%)";
+}      
+}
+function close1() {
+let close = document.getElementById("slide");
+if (slide.style.width = "150px") {
+    slide.style.width = "0px";
+    opacity.style.filter = "brightness(100%)";
+}
+}
+function logo() {
+window.open("index.html",'_parent');
+}
+function resume() {
+window.open("Resume.html",'_parent');
+}
+function gallery() {
+window.open("gallery.html",'_parent');
+}
+function biodata() {
+window.open("biodata.html",'_parent');
+}
+function maile() {
+window.open("mailto: dharparmar2004@gmail.com",'_parent');
+}
+function mobile() {
+window.open("tel:9173189378",'_parent');
+}
+function instagram () {
+window.open("https://instagram.com/dharmik8530/",'_parent');
+}
+function facebook() {
+    window.open("https://www.facebook.com/parmar.dharmik.756?mibextid=ZbWKwL",'_parent');
+    }
+    function LinkedIn() {
+    window.open("https://www.linkedin.com/in/dharmik-parmar-5a87b6273",'_parent');
+    }
+    
+
